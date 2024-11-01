b69d4328d984b2f7d95e6bc849c576a836a5e635
jthig613 <67336532+jthig613@users.noreply.github.com>
2024-11-01 13:33:59 -0400

    InitialUpdate

---
+++ Source/Splyt/SplytGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SplytGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASplytGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASplytGameMode();
+};
+
+
+
