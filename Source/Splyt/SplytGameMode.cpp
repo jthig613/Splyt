8195d4505aec04763fa80c604652d1a6bd281f55
jthig613 <67336532+jthig613@users.noreply.github.com>
2024-11-01 15:09:41 -0400

    Update

--- Source/Splyt/SplytGameMode.cpp
+++ Source/Splyt/SplytGameMode.cpp
@@ -12,4 +12,30 @@ ASplytGameMode::ASplytGameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+	CurrentDimension = EDimensions::DimensionOne;
 }
+
+void ASplytGameMode::ToggleDimension()
+{
+	// Toggle between Dimension One and Dimension Two
+	CurrentDimension = (CurrentDimension == EDimensions::DimensionOne) ? EDimensions::DimensionTwo : EDimensions::DimensionOne;
+
+	// Log for debugging
+	UE_LOG(LogTemp, Warning, TEXT("Dimension toggled to: %s"), CurrentDimension == EDimensions::DimensionOne ? TEXT("Dimension One") : TEXT("Dimension Two"));
+
+	OnDimensionSwapped.Broadcast(CurrentDimension);
+}
+
+//void ASplytGameMode::DimensionToggle()
+//{
+//	if (CurrentDimension == EDimensions::DimensionTwo)
+//	{
+//		CurrentDimension = EDimensions::DimensionOne;
+//	}
+//	else
+//	{
+//		CurrentDimension = EDimensions::DimensionTwo;
+//	}
+//}
+
--- Source/Splyt/SplytGameMode.h
+++ Source/Splyt/SplytGameMode.h
@@ -6,13 +6,38 @@
 #include "GameFramework/GameModeBase.h"
 #include "SplytGameMode.generated.h"
 
+UENUM(BlueprintType)
+enum class EDimensions : uint8
+{
+	DimensionOne UMETA(DisplayName = "VerseDimension"),
+	DimensionTwo UMETA(DisplayName = "NuanceDimension")
+};
+
+//Create a delegate
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDimensionSwapped, EDimensions, NewDimension);
+
+
 UCLASS(minimalapi)
 class ASplytGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Rules", meta = (AllowPrivateAccess = true))
+	EDimensions CurrentDimension;
 
 public:
 	ASplytGameMode();
+
+	// Function to toggle the dimension
+	UFUNCTION(BlueprintCallable, Category = "Dimension")
+	void ToggleDimension();
+
+	EDimensions GetDimension() { return CurrentDimension; }
+
+	UPROPERTY(BlueprintAssignable, Category = "Dimension")
+	FOnDimensionSwapped OnDimensionSwapped;
+
+	
 };
 
 
